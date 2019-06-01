d78056e0a8a7bc199c2dd24664d3ebeb9468eb14
Vitaly Antonov <legidium@gmail.com>
2019-06-02 03:08:48 +0400

    Fix conext examples

--- src/context/index.js
+++ src/context/index.js
@@ -1,23 +1,19 @@
 import React from "react";
 import ThemeContext from "./theme-context";
-import UserContext from "./user-context";
 
 // import Button from "./button-function";
 // import Button from "./button-class";
-import Button from "./button-hook";
+// import Button from "./button-hook";
+import Button from "./button-hook-custom";
 
 const theme = { color: "blue" };
-
-const user = "mike";
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={user}>
-        <ThemeContext.Provider value={theme}>
-          <Button>Hello</Button>
-        </ThemeContext.Provider>
-      </UserContext.Provider>
+      <ThemeContext.Provider value={theme}>
+        <Button>Hello</Button>
+      </ThemeContext.Provider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-import App from "./state";
-// import App from "./context";
+// import App from "./state";
+import App from "./context";
 // import App from "./effect";
 
 ReactDOM.render(<App />, document.getElementById("root"));
